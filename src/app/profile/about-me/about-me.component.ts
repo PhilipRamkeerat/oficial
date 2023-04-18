4892f2f578d77aeb44b5a9de421662c4eac1e209
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2023-04-18 18:30:35 -0400

    fix: code refactor

--- src/app/profile/about-me/about-me.component.ts
+++ src/app/profile/about-me/about-me.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
   public name: string = 'Philip Ramkeerat';
-  public description: string = 'Olá, eu sou Philip Ramkeerat, um desenvolvedor Frontend Senior especializado no framework Angular. Com vários anos de experiência em projetos web, tenho todas as habilidades necessárias para atuar como um Frontend Senior Angular, desde o desenvolvimento de componentes complexos até a integração de APIs e a criação de testes unitários e de integração para garantir a qualidade do código.';
-
+  public description: string = `Hello, I'm Philip Ramkeerat, a Senior Frontend developer specialized in the Angular framework. With several years of experience in web projects, I have all the necessary skills to act as a Senior Angular Frontend.`;
   constructor() { }
 
   ngOnInit(): void {
