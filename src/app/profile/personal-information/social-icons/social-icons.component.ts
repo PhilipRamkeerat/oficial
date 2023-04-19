3f44669e6c595e04686b4d1b89c63c4ede124987
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2023-04-18 22:17:32 -0400

    feat: adjust social icons to better code

---
+++ src/app/profile/personal-information/social-icons/social-icons.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-icons',
+  templateUrl: './social-icons.component.html',
+  styleUrls: ['./social-icons.component.scss']
+})
+export class SocialIconsComponent {
+  socialMedia = [
+    {
+      link: 'https://www.instagram.com/phil.rkt/',
+      iconUrl: 'assets/images/social-icons/insta.png',
+      altText: 'Instagram'
+    },
+    // {
+    //   link: 'https://twitter.com/',
+    //   iconUrl: 'assets/images/social-icons/twitter.png',
+    //   altText: 'Twitter'
+    // },
+    // {
+    //   link: 'https://www.facebook.com/',
+    //   iconUrl: 'assets/images/social-icons/facebook.png',
+    //   altText: 'Facebook'
+    // },
+    {
+      link: 'https://www.linkedin.com/in/philip-ramkeerat/',
+      iconUrl: 'assets/images/social-icons/linkedin.png',
+      altText: 'LinkedIn'
+    },
+    // {
+    //   link: 'https://www.youtube.com/',
+    //   iconUrl: 'assets/images/social-icons/youtube.png',
+    //   altText: 'YouTube'
+    // },
+    {
+      link: 'https://github.com/PhilipRamkeerat',
+      iconUrl: 'assets/images/social-icons/github.svg',
+      altText: 'GitHub'
+    }
+  ];
+
+
+}
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PersonalInformationComponent } from './personal-information/personal-information.component';
+import { SocialIconsComponent } from './personal-information/social-icons/social-icons.component';
 
 
 
 @NgModule({
   declarations: [
     AboutMeComponent,
-    PersonalInformationComponent
+    PersonalInformationComponent,
+    SocialIconsComponent
   ],
   imports: [
     CommonModule
