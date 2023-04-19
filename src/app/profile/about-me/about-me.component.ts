39a5fddf9a108ecd45e75cdd26685a945dd39015
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2023-04-19 00:19:13 -0400

    feat: Implement hire button

--- src/app/profile/about-me/about-me.component.ts
+++ src/app/profile/about-me/about-me.component.ts
@@ -8,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class AboutMeComponent implements OnInit {
   public name: string = 'Philip Ramkeerat';
   public description: string = `Hello, I'm Philip Ramkeerat, a Senior Frontend developer specialized in the Angular framework. With several years of experience in web projects, I have all the necessary skills to act as a Frontend.`;
+  linkedInUrl = 'https://www.linkedin.com/in/philip-ramkeerat/';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public showMoreInfo(): void {
-    // Lógica para exibir mais informações do desenvolvedor
-    console.log('In Construction');
+  public hireEvent(): void {
+    const confirmation = confirm('Are you sure you want to go to LinkedIn to contact me for hiring my services?');
+    if (confirmation) {
+      window.open(this.linkedInUrl, '_blank');
+    }
   }
 }
