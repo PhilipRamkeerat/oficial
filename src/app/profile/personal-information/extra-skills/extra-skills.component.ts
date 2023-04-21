7c52e09781ca49d54292e17a2c79fcc225aa0bbe
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2023-04-21 14:51:17 -0400

    feat: Implements upgrade on extra-skills component

--- src/app/profile/personal-information/extra-skills/extra-skills.component.ts
+++ src/app/profile/personal-information/extra-skills/extra-skills.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+interface Skill {
+  icon: string;
+  name: string;
+}
 @Component({
   selector: 'app-extra-skills',
   templateUrl: './extra-skills.component.html',
   styleUrls: ['./extra-skills.component.scss']
 })
-export class ExtraSkillsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ExtraSkillsComponent {
+  skills: Skill[] = [
+    {
+      icon: 'assets/images/Icons.png',
+      name: 'HTML, CSS, SASS, LESS'
+    },
+    {
+      icon: 'assets/images/Icons.png',
+      name: 'TypeScript, JavaScript'
+    },
+    {
+      icon: 'assets/images/Icons.png',
+      name: 'Angular 2+'
+    },
+    {
+      icon: 'assets/images/Icons.png',
+      name: 'Bootstrap, Material Design'
+    },
+    {
+      icon: 'assets/images/Icons.png',
+      name: 'GIT, Agile Methodologies'
+    }
+  ];
 }
