150769a170d570d2d69afbd590f6f8f2a36fa75c
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2023-04-23 20:03:44 -0400

    feat: Implement galery component portfolio with categories to select

--- src/app/portfolio-galery/portfolio-galery.component.ts
+++ src/app/portfolio-galery/portfolio-galery.component.ts
@@ -3,29 +3,66 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-portfolio-galery',
   templateUrl: './portfolio-galery.component.html',
-  styleUrls: ['./portfolio-galery.component.scss']
+  styleUrls: ['./portfolio-galery.component.scss'],
 })
 export class PortfolioGaleryComponent {
   title = 'Portfolio';
-  description = 'Discover my web development projects, focused on user experience and built with best practices. Explore more!';
-  activeCategory: string = 'Web Development';
+  description =
+    'Discover my web development projects, focused on user experience and built with best practices. Explore more!';
+  activeCategory: string = 'All';
 
-  images: { src: string, alt: string }[] = [
-    { src: 'assets/images/300x300.png', alt: 'Image 1' },
-    { src: 'assets/images/300x300.png', alt: 'Image 2' },
-    { src: 'assets/images/300x300.png', alt: 'Image 3' },
+  images: { src: string; alt: string; url: string }[] = [
+    {
+      src: 'https://picsum.photos/300/300',
+      alt: 'Image 1',
+      url: 'https://www.google.com',
+    },
+    {
+      src: 'https://picsum.photos/300/300',
+      alt: 'Image 2',
+      url: 'https://www.google.com',
+    },
+    {
+      src: 'https://picsum.photos/300/300',
+      alt: 'Image 3',
+      url: 'https://www.google.com',
+    }
   ];
 
-  webDevImages: { src: string, alt: string }[] = [
-    { src: 'assets/images/300x300.png', alt: 'Image 1' },
-    { src: 'assets/images/300x300.png', alt: 'Image 2' },
-    { src: 'assets/images/300x300.png', alt: 'Image 3' },
+  webDevImages: { src: string; alt: string; url: string }[] = [
+    {
+      src: 'https://picsum.photos/300/300?grayscale',
+      alt: 'Image 1',
+      url: 'https://www.google.com',
+    },
+    {
+      src: 'https://picsum.photos/300/300?grayscale',
+      alt: 'Image 2',
+      url: 'https://www.google.com',
+    },
+    {
+      src: 'https://picsum.photos/300/300?grayscale',
+      alt: 'Image 3',
+      url: 'https://www.google.com',
+    },
   ];
 
-  uiUxImages: { src: string, alt: string }[] = [
-    { src: 'assets/images/ui-ux.jpg', alt: 'Image 4' },
-    { src: 'assets/images/ui-ux.jpg', alt: 'Image 5' },
-    { src: 'assets/images/ui-ux.jpg', alt: 'Image 6' },
+  uiUxImages: { src: string; alt: string; url: string }[] = [
+    {
+      src: 'https://picsum.photos/300/300?sepia',
+      alt: 'Image 4',
+      url: 'https://www.google.com',
+    },
+    {
+      src: 'https://picsum.photos/300/300?sepia',
+      alt: 'Image 5',
+      url: 'https://www.google.com',
+    },
+    {
+      src: 'https://picsum.photos/300/300?sepia',
+      alt: 'Image 6',
+      url: 'https://www.google.com',
+    },
   ];
 
   changeCategory(category: string) {
@@ -38,9 +75,9 @@ export class PortfolioGaleryComponent {
         return this.webDevImages;
       case 'UI/UX Design':
         return this.uiUxImages;
+      case 'All':
       default:
         return this.images;
     }
   }
 }
-
