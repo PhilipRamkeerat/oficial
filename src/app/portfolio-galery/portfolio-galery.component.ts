967bc98e60d3876facb32ce616b174c72cebed12
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2023-04-23 20:09:45 -0400

    feat: Document methods from portfolio galery

--- src/app/portfolio-galery/portfolio-galery.component.ts
+++ src/app/portfolio-galery/portfolio-galery.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio-galery.component.scss'],
 })
 export class PortfolioGaleryComponent {
+  // Define the properties of the portfolio
   title = 'Portfolio';
   description =
     'Discover my web development projects, focused on user experience and built with best practices. Explore more!';
+
+  // Set the default active category to be "All"
   activeCategory: string = 'All';
 
+  // Define an array of images with their source URL, alternative text, and URL for redirection
   images: { src: string; alt: string; url: string }[] = [
     {
       src: 'https://picsum.photos/300/300',
@@ -29,6 +33,7 @@ export class PortfolioGaleryComponent {
     }
   ];
 
+  // Define an array of images for the "Web Development" category
   webDevImages: { src: string; alt: string; url: string }[] = [
     {
       src: 'https://picsum.photos/300/300?grayscale',
@@ -44,9 +49,10 @@ export class PortfolioGaleryComponent {
       src: 'https://picsum.photos/300/300?grayscale',
       alt: 'Image 3',
       url: 'https://www.google.com',
-    },
+    }
   ];
 
+  // Define an array of images for the "UI/UX Design" category
   uiUxImages: { src: string; alt: string; url: string }[] = [
     {
       src: 'https://picsum.photos/300/300?sepia',
@@ -62,13 +68,15 @@ export class PortfolioGaleryComponent {
       src: 'https://picsum.photos/300/300?sepia',
       alt: 'Image 6',
       url: 'https://www.google.com',
-    },
+    }
   ];
 
+  // Define a function to change the active category
   changeCategory(category: string) {
     this.activeCategory = category;
   }
 
+  // Define a function to get the images for the current active category
   getImagesForCategory() {
     switch (this.activeCategory) {
       case 'Web Development':
